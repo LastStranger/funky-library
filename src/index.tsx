f2a3f0cca57d1da98c9f4dc4e23823bad978139e
李振 <lizhen@hujiang.com>
2020-10-27 11:34:23 +0800

    change index in dev

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,6 @@ interface Props {
 }
 
 export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component one: {text}</div>
+  return <div className={styles.test}>Component one: {text}</div>
 }
 export { default as FunkyBar } from './components/funkyBar'
